972e256448be13653ae9236501e2556f58eb4f4d
”f2f2562” <126849568+f2f256f@users.noreply.github.com>
2023-06-15 05:05:07 +0900

    section2

---
+++ w15/wrapUp/stp4/script/intersectionObserver.js
@@ -0,0 +1,25 @@
+let scrollOption = {
+  root: null,
+  rootMargin: "200px",
+  threshold: 0.75,
+};
+
+let scrollFunction = (scrollTargets) => {
+  scrollTargets.forEach((eachscrollTarget) => {
+    if (eachscrollTarget.isIntersecting) {
+      eachscrollTarget.target.classList.add("triggered");
+    } else {
+      eachscrollTarget.target.classList.remove("triggered");
+    }
+  });
+};
+
+let scroll = new IntersectionObserver(scrollFunction, scrollOption);
+
+let elems = document.querySelectorAll(".animTrigger");
+
+console.log(elems);
+
+elems.forEach((eachElem) => {
+  scroll.observe(eachElem);
+});
